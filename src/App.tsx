532591320adf89a0752c16af95dc6ca47f52d3b3
Luis Fernando Solano Martínez <luisfer.sm15@gmail.com>
2022-08-13 20:39:23 -0600

    :lipstick: Adding font family and assets.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Typography } from '@mui/material';
 const App = ()  => {
   return (
     <div className='flex w-full bg-blue-400 h-screen justify-center items-center'>
-      <Typography variant="h1" component="h2">
+      <Typography variant="h1" component="h2" style={{fontFamily: 'Poppins-Bold'}}>
         Hola Mundo
       </Typography>
     </div>
